23d3f50180ce6552312315242a291711c90bac97
Alexey Smorkalov <magen-ta@yandex.ru>
2015-08-09 21:25:04 +0400

    Week2 GreetFriend with ListActivity

--- GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -1,78 +1,39 @@
 package hk.ust.cse.comp107x.greetfriend;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.EditText;
 
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ListActivity {
 
     Button greetButton;
+    String[] names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        greetButton = (Button) findViewById(R.id.greetButton);
-        greetButton.setOnClickListener(this);
+        names = getResources().getStringArray(R.array.friends);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.friend_item, names));
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        TextView textView = (TextView) findViewById(R.id.textMessage);
-        EditText editText = (EditText) findViewById(R.id.editFriendName);
-
-        String friendName = editText.getText().toString();
-        switch (v.getId())
-        {
-            case R.id.greetButton:
-                Intent in = new Intent(this, ShowMessage.class);
-                in.putExtra("message", getString(R.string.greetString) + friendName + "!");
-                startActivity(in);
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    // Returns current hour
-    int GetCurrentHour()
-    {
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-        return hour;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent in = new Intent(this, ShowMessage.class);
+        in.putExtra("message", getString(R.string.greetString) + names[(int) id] + "!");
+        startActivity(in);
     }
 }
